d18151afd348d323a8b3b47c6dafd20155fb8e62
“fx67ll;git config --global user.name “fx67ll <2717745014@qq.com>
2026-02-10 16:24:55 +0800

    dev proxy

--- config/proxy.ts
+++ config/proxy.ts
@@ -1,15 +1,9 @@
 /**
  * 在生产环境 代理是无法生效的，所以这里没有生产环境的配置
- * -------------------------------
- * The agent cannot take effect in the production environment
- * so there is no configuration of the production environment
- * For details, please see
- * https://pro.ant.design/docs/deploy
  */
 export default {
   dev: {
     '/express-api/': {
-      // target: 'https://preview.pro.ant.design',
       // target: 'http://localhost:8001', // npm run start 可以走本地mock数据
       target: 'https://node.fx67ll.com',
       changeOrigin: true,
@@ -17,17 +11,19 @@ export default {
     },
   },
   test: {
-    // '/api/': {
-    //   target: 'https://preview.pro.ant.design',
-    //   changeOrigin: true,
-    //   pathRewrite: { '^': '' },
-    // },
+    '/express-api/': {
+      // target: 'http://localhost:8001', // npm run start 可以走本地mock数据
+      target: 'https://node.fx67ll.com',
+      changeOrigin: true,
+      pathRewrite: { '^': '' },
+    },
   },
   pre: {
-    // '/api/': {
-    //   target: 'your pre url',
-    //   changeOrigin: true,
-    //   pathRewrite: { '^': '' },
-    // },
+    '/express-api/': {
+      // target: 'http://localhost:8001', // npm run start 可以走本地mock数据
+      target: 'https://node.fx67ll.com',
+      changeOrigin: true,
+      pathRewrite: { '^': '' },
+    },
   },
 };
